cfb0fc2af49c445e1b5e9ae283816aef76aa35fb
あずにゅ～💖 <dotnewzero@gmail.com>
2018-08-03 20:58:35 +0200

    reordered Article fields

--- BlazorRss.Shared/Models/Article.cs
+++ BlazorRss.Shared/Models/Article.cs
@@ -8,17 +8,18 @@ namespace BlazorRss.Shared.Models
         public Feed Feed { get; set; }
         public string UniqueId { get; set; }
 
-        public bool Read { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
 
         public DateTimeOffset DatePublished { get; set; }
         public DateTimeOffset DateUpdated { get; set; }
 
         public string ArticleUrl { get; set; }
-        public string Author { get; set; }
-        public string Title { get; set; }
         public string Description { get; set; }
         public string Content { get; set; }
+        public string Tags { get; set; }
 
+        public bool Read { get; set; }
         public bool Deleted { get; set; }
     }
 }
