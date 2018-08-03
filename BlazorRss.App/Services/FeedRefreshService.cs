8c7c3cb5c47c188ac501976a994bda0f3edd92fe
あずにゅ～💖 <dotnewzero@gmail.com>
2018-08-03 18:40:08 +0200

    oops, wrong logic with checking for uniqueness of articles

--- BlazorRss.App/Services/FeedRefreshService.cs
+++ BlazorRss.App/Services/FeedRefreshService.cs
@@ -103,7 +103,7 @@ namespace BlazorRss.App.Services
                             case SyndicationElementType.Item:
                                 var item = await feedreader.ReadItem();
 
-                                if (feed.Articles.Where(x => x.UniqueId == item.Id && x.DateUpdated != item.LastUpdated).Count() > 0)
+                                if (feed.Articles.Where(x => x.UniqueId == item.Id && x.DateUpdated == item.LastUpdated).Count() > 0)
                                     break;
 
                                 var article = new Article
